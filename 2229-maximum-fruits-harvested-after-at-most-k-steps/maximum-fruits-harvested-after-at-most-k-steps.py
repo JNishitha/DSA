69f918deaa09cd583af2ca3ab2c4383b59ec7cdd
JNishitha <153930854+JNishitha@users.noreply.github.com>
2025-08-03 21:16:43 +0530

    Time: 139 ms (83.33%) | Memory: 48.3 MB (83.33%) - LeetSync

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maxTotalFruits(self, fruits, startPos, k):
+        """
+        :type fruits: List[List[int]]
+        :type startPos: int
+        :type k: int
+        :rtype: int
+        """
+        n = len(fruits)
+        maxFruits = 0
+        total = 0
+        left = 0
+
+        for right in range(n):
+            total += fruits[right][1]
+            while left <= right:
+                left_pos = fruits[left][0]
+                right_pos = fruits[right][0]
+                min_steps = min(
+                    abs(startPos - left_pos) + (right_pos - left_pos),  
+                    abs(startPos - right_pos) + (right_pos - left_pos)  
+                )
+
+                if min_steps <= k:
+                    break
+                total -= fruits[left][1]
+                left += 1
+
+            maxFruits = max(maxFruits, total)
+
+        return maxFruits
